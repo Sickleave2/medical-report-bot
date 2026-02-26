a78ddf20d59c41892492ad80b203cea0332783b0
Sickleave2 <amoryalaqlany@gmail.com>
2026-02-26 04:04:19 +0300

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -5,14 +5,16 @@ class Navigation:
     @staticmethod
     async def push_state(state: FSMContext, new_state: str):
         """إضافة حالة جديدة إلى المكدس"""
-        stack = await state.get_data('nav_stack', [])
+        data = await state.get_data()
+        stack = data.get('nav_stack', [])
         stack.append(new_state)
         await state.update_data(nav_stack=stack)
 
     @staticmethod
     async def pop_state(state: FSMContext) -> str:
         """إزالة آخر حالة وإرجاعها"""
-        stack = await state.get_data('nav_stack', [])
+        data = await state.get_data()
+        stack = data.get('nav_stack', [])
         if stack:
             return stack.pop()
         return None
@@ -20,13 +22,12 @@ class Navigation:
     @staticmethod
     async def go_back(state: FSMContext, current_state: str) -> str:
         """الرجوع للخلف: يزيل الحالة الحالية ويعيد الحالة السابقة"""
-        stack = await state.get_data('nav_stack', [])
+        data = await state.get_data()
+        stack = data.get('nav_stack', [])
         if not stack:
             return None
         # الحالة السابقة هي آخر عنصر في المكدس
-        previous = stack[-1]
-        # لا نزيلها هنا، بل سنقوم بالرجوع إليها
-        return previous
+        return stack[-1]
 
     @staticmethod
     async def reset(state: FSMContext):
